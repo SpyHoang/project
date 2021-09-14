1f0ddb9486a00f1cb07f815f0624114dcffc3f7d
SpyHoang <62069452+SpyHoang@users.noreply.github.com>
2021-09-14 11:25:33 +0700

    Update dangnhap.php

--- pages/main/dangnhap.php
+++ pages/main/dangnhap.php
@@ -5,35 +5,74 @@ if (isset($_POST['dangnhap'])) {
     $sql = "SELECT * FROM tbl_dangky WHERE email='" . $email . "' AND matkhau='" . $matkhau . "' LIMIT 1";
     $row = mysqli_query($mysqli, $sql);
     $count = mysqli_num_rows($row);
-
     if ($count > 0) {
         $row_data = mysqli_fetch_array($row);
         $_SESSION['dangky'] = $row_data['tenkhachhang'];
         $_SESSION['id_khachhang'] = $row_data['id_dangky'];
-        header("Location:index.php?main=index.php");
+        // header("Location:index.php?quanly=giohang");
+?>
+        <script>
+            window.location.href = "index.php?quanly=giohang";
+        </script>
+<?php
     } else {
         echo '<p style="color:red">Mật khẩu hoặc Email sai ,vui lòng nhập lại.</p>';
     }
 }
 ?>
-<form action="" autocomplete="off" method="POST">
-    <table border="1" width="50%" class="table-login" style="text-align: center;border-collapse: collapse;">
-        <tr>
-            <td colspan="2">
-                <h3>Đăng nhập khách hàng</h3>
-            </td>
-        </tr>
-        <tr>
-            <td>Tài khoản</td>
-            <td><input type="text" size="50" name="email" placeholder="Email..."></td>
-        </tr>
-        <tr>
-            <td>Mật khẩu</td>
-            <td><input type="password" size="50" name="password" placeholder="Mật khẩu..."></td>
-        </tr>
-        <tr>
+<!--div class="modal" id="modal">
+        <div class="modal__overlay" id="modal__overlay"></div>
+        <div class="modal__body"-->
+        
+            <!-- login form -->
+            <form class="auth-form" id="auth-form-log-in" action="" autocomplete="off" method="POST">
+                <div class="auth-form__container">
+                    <div class="auth-form__header">
+                        <h3 class="auth-form__heading">Đăng nhập</h3>
+                        <span class="auth-form__switch-btn" id="auth-form__switch-btn-register">
+                            <a href="index.php?quanly=dangky">Đăng ký</a>
+                        </span>
+                    </div>
 
-            <td colspan="2"><input type="submit" name="dangnhap" value="Đăng nhập"></td>
-        </tr>
-    </table>
-</form>
+                    <div class="auth-form__form">
+                        <div class="auth-form__group">
+                            <input class="auth-form__input" type="text" name="email" onblur="checkEmail()" id="text_email" placeholder="Email của bạn" required>
+                        </div>
+                        <div class="auth-form__group">
+                            <input class="auth-form__input" type="password" onblur="checkPass()" id="text_pass" name="password" placeholder="Mật khẩu của bạn" required>
+                        </div>
+                    </div>
+
+                    <div class="auth-form__aside">
+                        <div class="auth-form-help">
+                            <a href="#" class="auth-form__help-link auth-form-help-fogot">Quên mật khẩu</a>
+                            <span class="auth-form-help-separate">
+                            </span>
+                            <a href="#" class="auth-form__help-link">Cần trợ giúp?</a>
+                        </div>
+                    </div>
+
+                    <div class="auth-form__controls">
+                        <button class="btn auth-form__controls-back" id="auth-form__controls-back-2">TRỞ LẠI</button>
+                        <button class="btn btn--primary" name="dangnhap">Đăng nhập</button>
+                    </div>
+                </div>
+
+                <div class="auth-form__socials">
+                    <a href="#" class="btn btn--with-icon btn--size-s auth-form__socials--facebook">
+                        <i class="fab fa-facebook-square auth-form__socials-icon"></i>
+                        <span class="auth-form__socials-title">
+                            Đăng nhập với Facebook
+                        </span>
+                    </a>
+                    <a href="#" class="btn btn--with-icon btn--size-s auth-form__socials--google">
+                        <i class="fab fa-google auth-form__socials-icon"></i>
+                        <span class="auth-form__socials-title">
+                            Đăng nhập với google
+                        </span>
+                    </a>
+                </div>
+            </form>
+            <script src="/assets/js/index.js"></script>
+        <!--/div>
+</div-->
